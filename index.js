b2e15a1d9e15e5d124c5b5e2dc7e4284df79f6f1
vatiza <rakibur.mac@gmail.com>
2024-08-09 23:26:34 +0600

    vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
 
     const jobPosts = client.db("hireHubDB").collection("jobsPost");
